a9f4ee0404e38d24f8a60741808a48760c193a3f
vijaykumbhar15 <vijaykumbhar15@gmail.com>
2019-04-02 11:12:37 +0530

    First Commit - After configuring the Project

---
+++ OffersOcean/src/main/java/com/OffersOcean/app/OffersOceanApplication.java
@@ -0,0 +1,13 @@
+package com.OffersOcean.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffersOceanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffersOceanApplication.class, args);
+	}
+
+}
